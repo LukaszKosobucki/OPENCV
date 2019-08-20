589258556acd956c4b726e5ee8eb6e3339c00d3a
HimikoV <48067467+HimikoV@users.noreply.github.com>
2019-08-20 16:12:23 +0200

    Update dron.py

--- Python/dron.py
+++ Python/dron.py
@@ -19,14 +19,13 @@ while True:
     cv2.dilate(mask,kernel=None,dst=mask,iterations=3)
     output=cv2.bitwise_and(frame,frame,mask=mask)
     kupa1,kupa2=cv2.findContours(mask, mode=cv2.RETR_CCOMP,method= cv2.CHAIN_APPROX_SIMPLE)
-
-    if np.absolute(xOLD-kupa1[0][0][0][0])<100 and np.absolute(yOLD-kupa1[0][0][0][1])<100:
-        try:
+    try:
+        if np.absolute(xOLD-kupa1[0][0][0][0])<100 and np.absolute(yOLD-kupa1[0][0][0][1])<100:
             cv2.rectangle(frame,(kupa1[0][0][0][0]-80,kupa1[0][0][0][1]-60),(kupa1[0][0][0][0]+50,kupa1[0][0][0][1]+20),(0,0,0),5)
             xOLD=kupa1[0][0][0][0]
             yOLD=kupa1[0][0][0][1]
-        except:
-            print("nic")
+    except:
+        print("nic")
     if cv2.waitKey(5) & 0xFF ==ord('q'):
         break
 
